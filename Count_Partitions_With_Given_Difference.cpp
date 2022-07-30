399c1f23c067c145da98b7445f58e870e07662a5
sudhanshu <r02.sudhanshu@gmail.com>
2022-07-30 18:45:07 +0530

    Partitions With Given Difference

---
+++ Count_Partitions_With_Given_Difference.cpp
@@ -0,0 +1,16 @@
+/*
+s1 + s2 = tot
+s1 - s2 = d
+s1 - (tot - s1) = d
+2s1 - tot = d
+s1 = (tot-d)/2
+so we simply have to count subsets with given sum
+//corner case: 
+tot-d >= 0
+tot-d should be even
+
+to now the question is exactly same as number_of_subsets
+
+//https://www.codingninjas.com/codestudio/problems/partitions-with-given-difference_3751628
+
+*/
